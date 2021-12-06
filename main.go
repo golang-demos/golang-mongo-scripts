0a5109f60e2dc87a2ee13010e0ff4c6935192168
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-07 02:34:59 +0530

    Initial commit with get people API

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty"  bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty"  bson:"lastname,omitempty"`
+}
+
+var client *mongo.Client
+var DB_NAME string = "persondb"
+var MONGO_URI string = "mongodb://mongoadmin:secret@localhost:27017/?authSource=admin"
+
+func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Content-Type", "application/json")
+	var people []Person
+	collection := client.Database(DB_NAME).Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var person Person
+		cursor.Decode(&person)
+		people = append(people, person)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(people)
+}
+
+func main() {
+	fmt.Println("Starting the application")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI(MONGO_URI))
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+
+	http.ListenAndServe(":12345", router)
+}
