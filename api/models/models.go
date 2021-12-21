3e868f8656f98d0639f7fc1b4205bb66c7812e52
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-22 01:59:08 +0530

    Added type defintion for documents

---
+++ api/models/models.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Employes struct {
+	ID          primitive.ObjectID  `json:"_id" bson:"_id"`
+	Name        string              `json:"name" bson:"name"`
+	Designation string              `json:"designation" bson:"designation"`
+	ProjectID   primitive.ObjectID  `json:"project_id" bson:"project_id"`
+	Ctc         float64             `json:"ctc" bson:"ctc"`
+	Experience  int16               `json:"experience" bson:"experience"`
+	IsVerified  bool                `json:"isverified" bson:"isverified"`
+	JoinDate    primitive.Timestamp `json:"joindate" bson:"joindate"`
+	ShiftId     uint8               `json:"shiftid" bson:"shiftid"`
+}
+
+type Projects struct {
+	ID   primitive.ObjectID `json:"_id" bson:"_id"`
+	Name string             `json:"name" bson:"name"`
+}
