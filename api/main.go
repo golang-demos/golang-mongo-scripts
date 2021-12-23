4605c9ef298c16be182218d7b22de48441e215fd
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-24 02:22:10 +0530

    Improved Menu traversal logic

--- api/main.go
+++ api/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/golang-demos/golang-mongo-scripts/seeder"
 
 	"github.com/joho/godotenv"
+	"github.com/ttacon/chalk"
 )
 
 func main() {
@@ -47,23 +48,34 @@ func main() {
 	for {
 		index = 0
 		for _, dScript := range demoScripts {
-			fmt.Printf("\n[%d] %s", index+1, dScript.Name)
+			fmt.Printf("\n %2d : %s", index+1, dScript.Name)
 			index++
 		}
-		fmt.Printf("\n[%d] %s", 0, "Exit")
-		fmt.Print("\n\nOption : ")
+		fmt.Printf("\n %2d : %s", 0, "Exit")
+		fmt.Print("\n\nEnter Option : ")
 		fmt.Scan(&input)
 
-		inputIndex, _ := strconv.Atoi(input)
+		// convert input in number
+		inputIndex, err := strconv.Atoi(input)
+		if err != nil {
+			log.Print(chalk.Red, "Please enter a number between 0 to ", (index - 1), chalk.Reset)
+			continue
+		}
+
+		// Exit if 0 is entered
 		if inputIndex == 0 {
 			fmt.Print("\nExiting...")
 			break
 		}
 
-		if len(demoScripts) < inputIndex {
-			fmt.Print("\nWrong Input. Try again")
+		// Detect wrong input number. Reiterate if found.
+		if len(demoScripts) < inputIndex || inputIndex < 0 {
+			fmt.Print("\n", chalk.Red, "Wrong Input. Try again", chalk.Reset)
 			continue
 		}
+
+		// Execute selected Handler
+		demoScripts[inputIndex-1].Handler()
 	}
 
 }
